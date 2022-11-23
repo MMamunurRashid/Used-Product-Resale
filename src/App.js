ef145dd337c8b2b08b1dbeb642c29772b128a30f
Mamunur-Rashid <mdmamun.iubat.m@gmail.com>
2022-11-23 23:33:31 +0600

    router setup and navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Home from "./Pages/Home/Home/Home";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes/Routes";
 
 function App() {
   return (
-    <div className="">
-      <Home></Home>
+    <div className="max-w-[1440px] mx-auto">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,15 +1,11 @@
 import React from "react";
 import useTitle from "../../../hooks/useTitle";
-import Navbar from "../../Shared/Navbar/Navbar";
-import Footer from "../../Shared/Footer/Footer";
 
 const Home = () => {
   useTitle("Home");
   return (
     <div>
-      <Navbar></Navbar>
       <h1>Home</h1>
-      <Footer></Footer>
     </div>
   );
 };
